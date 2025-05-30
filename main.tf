cc8857a3edb0f31b8eedda569bcb63708edbc78d
Sushama Varma <75479374+sushama04@users.noreply.github.com>
2025-05-30 20:03:51 +0530

    Create main.tf
    create eks cluster

---
+++ main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+locals {
+  region          = "ap-south-1"
+  name            = "flask_app_cluster"
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+
+  tags = {
+    Example = local.name
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.13.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                          = 1
+    "kubernetes.io/cluster/${local.name}"             = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"                 = 1
+    "kubernetes.io/cluster/${local.name}"             = "shared"
+  }
+
+  tags = local.tags
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
+
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  eks_managed_node_groups = {
+    panda-node = {
+      min_size       = 2
+      max_size       = 4
+      desired_size   = 2
+      instance_types = ["t2.medium"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "Flask_Node"
+      }
+    }
+  }
+
+  tags = local.tags
+}
